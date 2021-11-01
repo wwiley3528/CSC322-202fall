5491d061013a6bff5c7306262c70af8ed1f8773c
William Wiley <wwiley3528@my.sullivan.edu>
2021-10-31 21:36:04 -0400

    Updated power.js

--- powers.js
+++ powers.js
@@ -1,17 +1,20 @@
-<!DOCTYPE ;html>
+<!DOCTYPE html>
 <html>
 <body>
 
 <script>
 var s = "Numbers: ";
-for(var i=5; i<=15 ;i++)
+for(var i=5; i<=15 ;i++) {
 s = s + i + " ";
+}
 s = s+"\nSquares: ";
-for(var i=5; i<=15 ;i++) 
+for(var i=5; i<=15 ;i++) {
 s = s + (i* i) + " ";
+}
 s = s+"\nCubes: ";
-for(var i=5; i<=15 ;i++)
+for(var i=5; i<=15 ;i++) {
 s = s + (i* i*i) + " ";
+}
 alert(s);
 </script>
 
