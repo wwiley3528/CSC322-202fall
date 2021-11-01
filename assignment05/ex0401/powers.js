64219f02b14c3f8551490be7c2ec31773fddbc09
William Wiley <wwiley3528@my.sullivan.edu>
2021-10-31 21:55:06 -0400

    Update

---
+++ assignment05/ex0401/powers.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+var s = "Numbers: ";
+for(var i=5; i<=15 ;i++) {
+s = s + i + " "
+}
+s = s+"\nSquares: ";
+for(var i=5; i<=15 ;i++) {
+s = s + (i* i) + " "
+}
+s = s+"\nCubes: ";
+for(var i=5; i<=15 ;i++) {
+s = s + (i* i*i) + " "
+}
+alert(s);
+</script>
+
+</body>
+</html>
