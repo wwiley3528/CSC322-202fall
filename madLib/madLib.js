61dd7862763cad99315ddb66c0224c6bb5cd2a85
William Wiley <wwiley3528@my.sullivan.edu>
2021-11-07 17:38:23 -0500

    MadLib added

---
+++ madLib/madLib.js
@@ -0,0 +1,12 @@
+var firstName = prompt("Enter a first name: ");
+var lastName = prompt("Enter a last name: ");
+var occupation = prompt("Enter an occupation: ");
+var salary = prompt("Enter a number between 1000 and 5000: ");
+var yourExp = prompt("Enter a saying or expression: ");
+var verb = prompt("Enter an action verb not ending in ING: ");
+var adjective = prompt("Enter an adjective: ");
+var pluralNoun = prompt("Enter a plural noun: ");
+var car = prompt("Enter your favorite car: ");
+var color = prompt("Enter a color: ");
+
+document.write(firstName + lastName + "just got offered a new job as a" + occupation + "making" + salary + "dollars per month." ,yourExp + "," ,"you say! Your job requires you to" + verb + "and make" + adjective + "" + pluralNoun + "." ,"The plan is to purchase a new" + color + car + ".");
