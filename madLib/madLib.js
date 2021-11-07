ff6f7202461194eb41ee34953a7849ef705217e3
William Wiley <wwiley3528@my.sullivan.edu>
2021-11-07 18:34:22 -0500

    Updated

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -1,12 +1,38 @@
-var firstName = prompt("Enter a first name: ");
-var lastName = prompt("Enter a last name: ");
-var occupation = prompt("Enter an occupation: ");
-var salary = prompt("Enter a number between 1000 and 5000: ");
-var yourExp = prompt("Enter a saying or expression: ");
-var verb = prompt("Enter an action verb not ending in ING: ");
-var adjective = prompt("Enter an adjective: ");
-var pluralNoun = prompt("Enter a plural noun: ");
-var car = prompt("Enter your favorite car: ");
-var color = prompt("Enter a color: ");
+        <meta charset= "utf-8">
+        <meta name="viewport" content="width=device-width, intial-scal=1.0">
+        <title>Javascript Mad Libs</title>
+        <link href="css/madLid.css />
 
-document.write(firstName + lastName + "just got offered a new job as a" + occupation + "making" + salary + "dollars per month." ,yourExp + "," ,"you say! Your job requires you to" + verb + "and make" + adjective + "" + pluralNoun + "." ,"The plan is to purchase a new" + color + car + ".");
+    <body>
+        <h1>Mad Libs</h1>
+        
+        <script type="text/javascript">
+            var firstName;
+            var lastName;
+            var occupation;
+            var salary;
+            var yourExp;
+            var verb;
+            var adjective;
+            var pluralNoun;
+            var color;
+            var car;
+
+            firstName=window.prompt("Enter a first name: ");
+            lastName=window.prompt("Enter a last name: ");
+            occupation=window.prompt("Enter an occupation: ");
+            salary=window.prompt("Enter a number between 1000 and 5000: ");
+            yourExp=window.prompt("Enter a saying or expression: ");
+            verb=window.prompt("Enter an action verb not ending in ING: ");
+            adjective=window.prompt("Enter an adjective: ");
+            pluralNoun=window.prompt("Enter a plural noun: ");
+            car=window.prompt("Enter your favorite car: ");
+            color=window.prompt("Enter a color: ");
+
+            document.write(firstName + lastName + “just got offered a new job as a” + occupation + “making” + salary + “dollars per month.” “ + yourExp +”, “ you say! Your job requires you to” + verb + “and make + adjective + “ “ + pluralNoun + “.” “They plan to purchase a new” + color + car “.”);
+            
+            </script>
+            
+    </body>
+
+
